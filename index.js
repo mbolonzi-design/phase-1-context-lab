91ae847f20d89b7201f0c6fbb1b29998b98db9b3
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-19 11:56:52 +0300

    commit tree 1bb3a36d6d8cb698a44d90207b6979858762f58a

--- index.js
+++ index.js
@@ -8,7 +8,66 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ function createEmployeeRecord ([firstName, familyName , title , payPerHour]) { 
+    let empObj = {
+        firstName : firstName,
+        familyName : familyName,
+        title : title,
+        payPerHour : payPerHour,
+        timeInEvents : [],
+        timeOutEvents: [],
+    }
+    return empObj
+}
+function createEmployeeRecords (recordsArray){
+    return recordsArray.map(createEmployeeRecord)
+}
 
+const createTimeInEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+
+const createTimeOutEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+
+const hoursWorkedOnDate = function (date){
+    const inEvent = this.timeInEvents.find(inEvent=>inEvent.date === date);
+    const outEvent = this.timeOutEvents.find(outEvent=>outEvent.date === date);
+            return ((outEvent.hour - inEvent.hour)/100)
+};
+
+const wagesEarnedOnDate = function (date){
+    return hoursWorkedOnDate.apply(this, [date])* this.payPerHour
+};
+
+
+
+
+function calculatePayroll(recordsArray){
+   let payroll = recordsArray.reduce((acc, cur)=> {
+       return acc + allWagesFor.call(cur)
+   }, 0)
+   return payroll
+    }
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let emp = srcArray.find((rec) => rec.firstName === firstName)
+    return emp;
+}
+ 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
